42e3d91a77c08df2a19d6e0bc2262b4dd8ec0ea1
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-21 12:02:43 +0300

    static root path for react front

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log(`user pass in env file is: ${process.env.MONGO_CONN_STRING}`);
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const cors = require('cors');
 
@@ -20,6 +21,12 @@ app.use(express.json());
 const todoApi = require('./api/todoApi');
 
 app.use('/', todoApi);
+
+const rootBuild = path.join(__dirname, 'client', 'build');
+// pasitikrinti ar musu aplinka yra production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(rootBuild));
+}
 
 // prisijungimas prie duomenu bazes
 mongoose
